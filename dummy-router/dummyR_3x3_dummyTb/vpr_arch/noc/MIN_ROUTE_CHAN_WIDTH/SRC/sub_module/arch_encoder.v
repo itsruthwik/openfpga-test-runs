c233be1f98ac85e901c756e79c1640a57277498f
Ruthwik Reddy Sunketa <ruthwik@debian.itsru.com>
2024-08-06 13:35:06 -0700

    Add an openfpga run files with dummy rtl as hard block and 3x3 noc as benchmark

---
+++ dummy-router/dummyR_3x3_dummyTb/vpr_arch/noc/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Decoders for fabric configuration protocol
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Tue Aug  6 13:30:05 2024
+//-------------------------------------------
