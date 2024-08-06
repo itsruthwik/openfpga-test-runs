dc72c511988306c34771dd1a68dfcfac38b844a2
Ruthwik Reddy Sunketa <ruthwik@debian.itsru.com>
2024-08-06 12:51:14 -0700

    Add an openfpga run with dummy router rtl as hard block

---
+++ dummy-router/dummyR_1x1_dummyTb/SRC/fpga_defines.v
@@ -0,0 +1,9 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Tue Aug  6 12:06:41 2024
+//-------------------------------------------
+`define ENABLE_TIMING 1
+
---
+++ dummy-router/dummyR_1x1_dummyTb/SRC/sub_module/shift_register_banks.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Shift register banks used in FPGA
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Tue Aug  6 12:06:41 2024
+//-------------------------------------------
