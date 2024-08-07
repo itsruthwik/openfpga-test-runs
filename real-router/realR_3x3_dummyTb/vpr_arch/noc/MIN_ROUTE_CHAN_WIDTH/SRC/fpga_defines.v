af479ab20db86c2f0cf1dda855f7deafa5b7d33b
Ruthwik Reddy Sunketa <ruthwik@debian.itsru.com>
2024-08-06 17:13:53 -0700

    Add openfpga run with Real router as hardblock and 3x3 noc as a benchmark

---
+++ real-router/realR_3x3_dummyTb/vpr_arch/noc/MIN_ROUTE_CHAN_WIDTH/SRC/fpga_defines.v
@@ -0,0 +1,9 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Tue Aug  6 17:07:42 2024
+//-------------------------------------------
+`define ENABLE_TIMING 1
+
---
+++ real-router/realR_3x3_dummyTb/vpr_arch/noc/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/local_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Local Decoders for Multiplexers
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Tue Aug  6 17:07:42 2024
+//-------------------------------------------
