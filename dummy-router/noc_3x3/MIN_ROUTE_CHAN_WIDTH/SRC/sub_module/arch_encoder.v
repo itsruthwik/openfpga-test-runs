a83f872673d1c091741cc6567de0cb164145f239
Ruthwik <itsruthwik@outlook.com>
2024-08-11 23:08:58 -0700

    openfpga run with 3x3 noc as benchmark. Removed optimization passes in yosys scripts

---
+++ dummy-router/noc_3x3/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Decoders for fabric configuration protocol
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Sun Aug 11 23:03:55 2024
+//-------------------------------------------
---
+++ dummy-router/noc_3x3/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/shift_register_banks.v
@@ -0,0 +1,7 @@
+//-------------------------------------------
+//	FPGA Synthesizable Verilog Netlist
+//	Description: Shift register banks used in FPGA
+//	Author: Xifan TANG
+//	Organization: University of Utah
+//	Date: Sun Aug 11 23:03:55 2024
+//-------------------------------------------
