97383d95c12bd32258a847f13ceaa66926945210
Ruthwik Reddy Sunketa (Student) <rsunketa@en4228283l.fulton.ad.asu.edu>
2024-10-01 17:42:24 -0700

    Remove old runs

--- dummy-router/3x3_r/vpr_arch/noc/MIN_ROUTE_CHAN_WIDTH/SRC/fpga_defines.v
+++
@@ -1,9 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Preprocessing flags to enable/disable features in FPGA Verilog modules
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Mon Sep  9 09:18:43 2024
-//-------------------------------------------
-`define ENABLE_TIMING 1
-
--- dummy-router/3x3_r/vpr_arch/noc/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/arch_encoder.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Decoders for fabric configuration protocol
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Mon Sep  9 09:18:43 2024
-//-------------------------------------------
