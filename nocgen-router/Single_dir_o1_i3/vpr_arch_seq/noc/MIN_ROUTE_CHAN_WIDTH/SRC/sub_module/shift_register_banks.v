42a6e490796fd79d6c7aacf44f215dfce829d64f
rsunketa <rsunketa@en4230429l.cidse.dhcp.asu.edu>
2024-12-08 22:36:58 -0700

    Removing previous task run

--- nocgen-router/Single_dir_o1_i3/vpr_arch_seq/noc/MIN_ROUTE_CHAN_WIDTH/SRC/sub_module/shift_register_banks.v
+++
@@ -1,7 +0,0 @@
-//-------------------------------------------
-//	FPGA Synthesizable Verilog Netlist
-//	Description: Shift register banks used in FPGA
-//	Author: Xifan TANG
-//	Organization: University of Utah
-//	Date: Thu Oct 17 11:38:19 2024
-//-------------------------------------------
