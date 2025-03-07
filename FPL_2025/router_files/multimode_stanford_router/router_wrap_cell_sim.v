c4ee7e86c583034207b59401456df5306aa07470
malifarooq <mafarooq19@asu.edu>
2025-03-07 16:59:10 -0700

    stuff

--- FPL_2025/router_files/multimode_stanford_router/router_wrap_cell_sim.v
+++ FPL_2025/router_files/multimode_stanford_router/router_wrap_cell_sim.v
@@ -5,14 +5,11 @@ module router_desc(
     input [0:3] router_address,
     input [0:189] channel_in_ip,      
     input [0:14] flow_ctrl_out_ip,
-    output [0:189] channel_out_op,     
-    output [0:14] flow_ctrl_in_op,
-    output error
+    output reg [0:189] channel_out_op,     
+    output reg [0:14] flow_ctrl_in_op,
+    output reg error
 ); 
 
-  reg [0:189] channel_out_op;
-  reg [0:14] flow_ctrl_in_op;
-  reg error;
   always @(posedge clk) begin
     if (reset) begin
       error <= 0;
@@ -30,18 +27,14 @@ module router_asc(
   input [0:3] router_address,
   input [0:189] channel_in_ip,
   output [0:14] flow_ctrl_out_ip,
-  output [0:189] channel_out_op,
+  output reg [0:189] channel_out_op,
   input [0:14] flow_ctrl_in_op,
-  output error
+  output reg error
 );
 
-  reg [0:189] channel_out_op;
-  reg [0:14] flow_ctrl_in_op;
-  reg error;
   always @(posedge clk) begin
     if (reset) begin
       error <= 0;
-      flow_ctrl_in_op <= 0;
       channel_out_op <= 0;  
     end
   end
@@ -55,18 +48,14 @@ module router_class(
   input [0:3] router_address,
   input [0:189] channel_in_ip,
   output [0:14] flow_ctrl_out_ip,
-  output [0:189] channel_out_op,
+  output reg [0:189] channel_out_op,
   input [0:14] flow_ctrl_in_op,
-  output error
+  output reg error
 );
 
-  reg [0:189] channel_out_op;
-  reg [0:14] flow_ctrl_in_op;
-  reg error;
   always @(posedge clk) begin
     if (reset) begin
       error <= 0;
-      flow_ctrl_in_op <= 0;
       channel_out_op <= 0;  
     end
   end
