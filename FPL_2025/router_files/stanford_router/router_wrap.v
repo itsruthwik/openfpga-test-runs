3012bd3ebaa3be04fd4a7bf8d9a15d9fbc1c3b96
Ruthwik Reddy Sunketa <itsruthwik@outlook.com>
2025-03-08 01:26:22 -0700

    changing port indices to [MSB:LSB] to support openfpga definition

--- FPL_2025/router_files/stanford_router/router_wrap.v
+++ FPL_2025/router_files/stanford_router/router_wrap.v
@@ -2,11 +2,11 @@
 module router_wrap(
         input clk,
         input reset,
-        input [0:3] router_address,
-        input [0:189] channel_in_ip,      
-        input [0:14] flow_ctrl_out_ip,
-        output [0:189] channel_out_op,     
-        output [0:14] flow_ctrl_in_op,
+        input [3:0] router_address,
+        input [189:0] channel_in_ip,      
+        output [14:0] flow_ctrl_out_ip,
+        output [189:0] channel_out_op,     
+        input [14:0] flow_ctrl_in_op,
         output error
     );
 
--- FPL_2025/router_files/stanford_router/router_wrap_cell_sim.v
+++ FPL_2025/router_files/stanford_router/router_wrap_cell_sim.v
@@ -2,26 +2,29 @@
 module router_wrap(
         input clk,
         input reset,
-        input [0:3] router_address,
-        input [0:189] channel_in_ip,      
-        input [0:14] flow_ctrl_out_ip,
-        output [0:189] channel_out_op,     
-        output [0:14] flow_ctrl_in_op,
+        input [3:0] router_address,
+        input [189:0] channel_in_ip,      
+        output [14:0] flow_ctrl_out_ip,
+        output [189:0] channel_out_op,     
+        input [14:0] flow_ctrl_in_op,
         output error
     );
 
-  reg [0:189] channel_out_op;
-  reg [0:14] flow_ctrl_in_op;
+  reg [189:0] channel_out_op;
+  reg [14:0] flow_ctrl_out_ip;
   reg error;
 
 
   always @(posedge clk) begin
     if (reset) begin
       error <= 0;
-      flow_ctrl_in_op <= 0;
+      flow_ctrl_out_ip <= 0;
       channel_out_op <= 0;  
+    end
+    else begin
+      channel_out_op <= channel_in_ip;
+      flow_ctrl_out_ip <= flow_ctrl_in_op;
     end
   end
         
-
 endmodule
